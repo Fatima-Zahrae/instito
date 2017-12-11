d3711f9e0c01b39e8368b1a143d5c3273e062c77
Fatima-Zahrae <fatimaelkessab17@gmail.com>
2017-12-11 10:41:33 +0100

    examenLoopback2017

---
+++ common/models/actividad.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(Actividad) {  
+    
+    
+    
+    
+     /*Actividad.beforeRemote('create', function (context, actividad, next) {
+        context.args.data.owner = context.req.accessToken.userId;
+         console.log(context.req.accessToken.userId);
+        next();
+    });*/
+};
